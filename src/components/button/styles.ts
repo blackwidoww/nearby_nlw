1861d2e8222862afce6f8fd33070b70e70f20c36
blackwidoww <sabrinaflor09@gmail.com>
2024-12-10 22:53:32 -0300

    teste

---
+++ src/components/button/styles.ts
@@ -0,0 +1,8 @@
+import { StyleSheet } from "react-native";
+import { colors, fontFamily } from "@/styles/theme"
+import { IconContainer } from "@tabler/icons-react-native";
+
+export const styles = StyleSheet.create({
+    container: {},
+    title: {}
+})
