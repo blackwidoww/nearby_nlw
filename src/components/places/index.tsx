3773602553f2736faaf81618fec324a45424b798
blackwidoww <sabrinaflor09@gmail.com>
2024-12-18 09:31:59 -0300

    finalizando a a estilização dos menus

--- src/components/places/index.tsx
+++ src/components/places/index.tsx
@@ -1,6 +1,10 @@
-import { View } from "react-native";
+import { Text, useWindowDimensions } from "react-native";
+import { useRef } from "react";
+import BottomSheet, { BottomSheetFlatList } from "@gorhom/bottom-sheet"
 
 import { Place, PlaceProps } from "../place";
+import { mayInitWithUrlAsync } from "expo-web-browser";
+import { styles } from "./styles";
 
 
 type Props = {
@@ -8,5 +12,31 @@ type Props = {
 }
 
 export function Places({ data }: Props) {
-    return <View></View>
+    const dimensions = useWindowDimensions()
+    const BottomSheetRef = useRef<BottomSheet>(null)
+
+    const snapPoints = {
+        min: 278,
+        max: dimensions.height - 128
+    }
+    return (
+    <BottomSheet
+    ref={BottomSheetRef}
+    snapPoints={[snapPoints.min, snapPoints.max]}
+    handleIndicatorStyle = {styles.indicator}
+    backgroundStyle = {styles.container}
+    enableOverDrag = {false}
+    >
+        <BottomSheetFlatList 
+        data={data}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <Place data={item} />}
+        contentContainerStyle={styles.content}
+        ListHeaderComponent={() => (
+            <Text style={styles.title}>Explore locais perto de você.</Text>
+        )}
+        showsVerticalScrollIndicator = {false}
+        />
+    </BottomSheet>
+    )
 }
